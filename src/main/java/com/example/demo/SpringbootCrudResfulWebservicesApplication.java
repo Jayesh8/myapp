da9088e40de55b5e58906aedc1e235b7dd9f0aa9
Jayesh8 <jayeshramani8@gmail.com>
2022-04-25 14:43:57 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootCrudResfulWebservicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudResfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudResfulWebservicesApplication.class, args);
+	}
+
+}
+  
